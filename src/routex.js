95f013e18c9a579515fdb1726d8b4c69f3eeceb9
Valeria Cossio <valeriacossio5@gmail.com>
2021-02-05 00:41:07 -0300

    Update Customer Management files

--- src/routex.js
+++ src/routex.js
@@ -3,7 +3,8 @@ import App from './App';
 import Home from './views/home'
 
 import { BrowserRouter as Router , Route } from 'react-router-dom';
-import KanbanBoard from './views/kanban';
+import KanbanBoard from './views/taskManagement';
+import CustomerManagement from './views/customerManagement'
 
 
 const Routes = () => (
@@ -11,6 +12,7 @@ const Routes = () => (
     <App>
       <Route exact path="/" component={Home} />
       <Route path="/kanban" component={KanbanBoard} />
+      <Route path="/customer" component={CustomerManagement} />
     </App>
   </Router>
 )
